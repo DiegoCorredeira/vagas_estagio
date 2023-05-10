73ed3d40493a4b2a2ece5e1bf9735dd2381ed368
Thainá Luiza <91692393+DiegoCorredeira@users.noreply.github.com>
2023-05-10 13:03:12 -0300

    new version

--- main.py
+++ main.py
@@ -1,76 +1,85 @@
+from tkinter import * 
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
 from time import sleep
 import pandas as pd
 
 
-'''
-Proximos passos: 
-- Ajustar o arquivo excel das vagas para a visualização ficar melhor.
-- Adicionar cartela de "clientes" para envio de email em massa
-- 
-
-'''
-
-'''
-Em busca da facilidade de buscar outras vagas além de estágio em TI
-adicionei essa implementação onde através dessas varáveis você consegue
-gerar o link de maneira mais eficaz - João Pedro Dutra
-'''
-
-keyword = "Estagio em TI" # Nome da vaga que busca - João Pedro Dutra
-location = "Brasil" # Localidade da vaga que busca - João Pedro Dutra
-geoid = "106057199" # Variável fixa enquanto focar apenas no Brasil - João Pedro Dutra
-
-URL_LINKEDIN_JOBS = f"https://www.linkedin.com/jobs/search/?geoId={geoid}&keywords={keyword}&location={location}&refresh=true"
-
-if __name__ == '__main__':
-    driver = webdriver.Chrome(ChromeDriverManager().install())
-    driver.maximize_window()  
-    driver.get(URL_LINKEDIN_JOBS)
-
-    results = driver.find_elements_by_class_name('base-card')
-    links = driver.find_elements_by_class_name('base-card')
-
-    descricao = []
-    empresa = []
-    local = []
-    titulo = []
-    sleep(15)
-    job_links = [link.get_attribute('href') for link in links]
-
-
-    while True:
-        for result in results[len(descricao):]:
-            result.click()
-            sleep(15)
-            try:
-                descricao.append(
-                    driver.find_element_by_class_name('description').text)
-                titulo.append(
-                    driver.find_element_by_class_name('topcard__title').text)
-                local.append(driver.find_element_by_class_name(
-                    'topcard__flavor--bullet').text)
-                empresa.append(
-                    driver.find_element_by_class_name('topcard__flavor').text)
-                print('Vaga salva com sucesso!')
-            except:
-                print("Erro ao capturar dados da vaga selecionada")
-
-        if len(descricao) == len(results):
-            break
+def buscar_vagas():
+    keyword = keyword_entry.get()
+    location = location_entry.get()
+    geoid = "106057199"
+    URL_LINKEDIN_JOBS = f"https://www.linkedin.com/jobs/search/?&keywords={keyword}&location={location}&refresh=true"
     
-
+    driver = webdriver.Chrome(ChromeDriverManager().install())
+    driver.maximize_window()  # mudar
+    driver.get(URL_LINKEDIN_JOBS)
+    
+    sleep(5)
+    
+    results = driver.find_elements(By.CSS_SELECTOR, '.base-card')
+    
+    output_text.delete(1.0, END)
+    
+    for i, result, in enumerate(results): 
+        if i >= 10: 
+            break
+        
+        result.click()
+        sleep(15)
+    
+        try: 
+            descricao = driver.find_element(By.CLASS_NAME, 'description').text
+            titulo = driver.find_element(By.CLASS_NAME, 'topcard__title').text
+            local = driver.find_element(By.CLASS_NAME, 'topcard__flavor--bullet').text
+            empresa = driver.find_element(By.CLASS_NAME, 'topcard__flavor').text
+            link = driver.current_url
+            
+            
+            output_text.insert(END, f'Título: {titulo}\n')
+            output_text.insert(END, f'Empresa: {empresa}\n')
+            output_text.insert(END, f'Local: {local}\n')
+            output_text.insert(END, f'Descrição: {descricao}\n')
+            output_text.insert(END, f'Link: {link}\n')
+            output_text.insert(END, f'------------------------------------------------------------------------\n')
+            
+            print('Vaga será exibida ao fechar navegador')
+            
+        except:
+            print('Erro ao tentar exibir ou capturar a vaga')
 
     driver.quit()
 
-    dados_coletados = {
-        'Título': titulo,
-        'Empresa': empresa,
-        'Local da Vaga': local,
-        'Descrição': descricao, 
-        'Link': job_links
-        }
-    df = pd.DataFrame(dados_coletados)
-    print(df.head())
-    df.to_excel('vagas.xlsx', encoding='utf-8')
+
+root = Tk()
+root.title('Buscador de vagas GUI')
+root.geometry("800x400")
+
+
+keyword_label = Label(root, text='Vaga: ')
+keyword_label.pack()
+keyword_entry = Entry(root)
+keyword_entry.pack()
+
+
+location_label = Label(root, text='Localidade: ')
+location_label.pack()
+location_entry = Entry(root)
+location_entry.pack()
+
+space_label = Label(root)
+space_label.pack()
+
+search_button = Button(root, text='Buscar', command=buscar_vagas)
+search_button.config(padx=150, pady=10)
+search_button.pack()
+
+space_label = Label(root)
+space_label.pack()
+
+output_text = Text(root)
+output_text.pack()
+
+
+root.mainloop()
